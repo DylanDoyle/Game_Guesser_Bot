e71cc9bd306ada614505830b594749bc7af374ec
Droo <droodoyle89@gmail.com>
2022-09-30 12:11:56 -0400

    Updated if statement style to match file

--- game_guesser_bot.py
+++ game_guesser_bot.py
@@ -186,7 +186,7 @@ async def weekly_scoreboard():
     # Use this week's scores to generate a message
     weekly_scoreboard_message = ("Here are this week's current standings:" + "\n\n")
     for (player_id, weekly_score) in zip(contents, contents[2:]):
-        if(weekly_score > 0):
+        if weekly_score > 0:
             weekly_scoreboard_message += "\t\t" + player_id + " - " + weekly_score + "\n"
     channel = bot.get_channel(target_channel_id)
     await channel.send(weekly_scoreboard_message)
